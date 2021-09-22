d109e730de1a7bd2afc0a718e49f9b00df48fa61
JpegzaCreate <jpegza@gmail.com>
2021-09-22 12:09:30 +0200

    Initial commit

---
+++ src/concatenaciones/Concatenaciones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concatenaciones;
+
+/**
+ *
+ * @author javip
+ */
+public class Concatenaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var alumno = "Lucía";
+        var titulo= "Informático";
+        var sueldo = 1200;
+        
+        var concat = titulo + " " + alumno +" "+ sueldo+"€";
+        System.out.println("concat = " + concat);
+        
+                
+
+
+    }
+    
+}
